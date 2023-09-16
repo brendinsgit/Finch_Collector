bb2533fffec5125294d8526cbf83103c40d48ba3
brendinsgit <139824521+brendinsgit@users.noreply.github.com>
2023-09-16 12:32:13 -0400

    finish finch-lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,25 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('finches/', views.finches_index, name='index'),
-    path('finches/<int:finch_id>', views.finches_detail, name='detail'),
+    path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
+    path('finches/create/', views.FinchCreate.as_view(), name='finch_create'),
+    path('finches/<int:pk>/update', views.FinchUpdate.as_view(), name='finch_update'),
+    path('finches/<int:pk>/delete', views.FinchDelete.as_view(), name='finch_delete'),
+    path('finches/<int:finch_id>/add_feeding', views.add_feeding, name='add_feeding'),
+
+
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+
+    path('toys/<int:toy_id>/update', views.ToyUpdate.as_view(), name='toys_update'),
+
+    path('toys/<int:toy_id>/delete', views.ToyDelete.as_view(), name='toys_delete'),
+
+
+    path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+
+    path('finches/<int:finch_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
-from .models import Finch
+from .models import Finch, Toy
 
-# data for testing purpose
-# finches = [
-#   {'breed': 'American Goldfinch', 'description': 'Breeding males are bright yellow with black forehead, black wings with white markings', 'origin': 'North America'},
-#   {'breed': 'Eurasian Bullfinch', 'description': 'The Eurasian bullfinch is a bulky bull-headed bird. The upper parts are grey; the flight feathers and short thick bill are black', 'origin': "Europe and Asia"},
-# ]
+from .forms import FeedingForm
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
@@ -21,4 +19,61 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+
+    id_list = finch.toys.all().values_list('id')
+
+    toys_doesnt_have = Toy.objects.exclude(id__in=id_list)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form, 'toys': toys_doesnt_have})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['breed', 'origin', 'description']
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['origin', 'description']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+
+class ToyList(ListView):
+    model = Toy
+    template_name = 'toys/index.html'
+
+class ToyDetail(DetailView):
+    model = Toy
+    template_name = 'toys/detail.html'
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys'
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
